95c5037a4cf2748298c010ff092be742b492e5d8
Abu Raihan Md Rahat Hossain <rahat.hossain@gmail.com>
2019-02-25 20:06:29 +1100

    AppConfig updated

--- src/main/scala/cluster/client/pubsub/AppConfig.scala
+++ src/main/scala/cluster/client/pubsub/AppConfig.scala
@@ -8,13 +8,64 @@ object AppConfig {
 
   val frontEndPortRange = 3000 to 3999
 
-  val singletonName1 = "pub-sub-master1"
-  val singletonRole1 = "pub-sub-back-end1"
-  val inTopic1 = "pub-sub-inTopic1"
-  val ResultsTopic1 = "pub-sub-outTopic1"
+  val singletonName1  = "pub-sub-master1"
+  val singletonRole1  = "pub-sub-back-end1"
+  val inTopic1        = "pub-sub-inTopic1"
+  val ResultsTopic1   = "pub-sub-outTopic1"
 
-  val singletonName2 = "pub-sub-master2"
-  val singletonRole2 = "pub-sub-back-end2"
-  val inTopic2 = "pub-sub-inTopic2"
-  val ResultsTopic2 = "pub-sub-outTopic2"
+  val singletonName2  = "pub-sub-master2"
+  val singletonRole2  = "pub-sub-back-end2"
+  val inTopic2        = "pub-sub-inTopic2"
+  val ResultsTopic2   = "pub-sub-outTopic2"
+
+  val singletonName3  = "pub-sub-master3"
+  val singletonRole3  = "pub-sub-back-end3"
+  val inTopic3        = "pub-sub-inTopic3"
+  val ResultsTopic3   = "pub-sub-outTopic3"
+
+  val singletonName4  = "pub-sub-master4"
+  val singletonRole4  = "pub-sub-back-end4"
+  val inTopic4        = "pub-sub-inTopic4"
+  val ResultsTopic4   = "pub-sub-outTopic4"
+
+  val singletonName5  = "pub-sub-master5"
+  val singletonRole5  = "pub-sub-back-end5"
+  val inTopic5        = "pub-sub-inTopic5"
+  val ResultsTopic5   = "pub-sub-outTopic5"
+
+  val singletonName6  = "pub-sub-master6"
+  val singletonRole6  = "pub-sub-back-end6"
+  val inTopic6        = "pub-sub-inTopic6"
+  val ResultsTopic6   = "pub-sub-outTopic6"
+
+  val singletonName7  = "pub-sub-master7"
+  val singletonRole7  = "pub-sub-back-end7"
+  val inTopic7        = "pub-sub-inTopic7"
+  val ResultsTopic7   = "pub-sub-outTopic7"
+
+  val singletonName8  = "pub-sub-master8"
+  val singletonRole8  = "pub-sub-back-end8"
+  val inTopic8        = "pub-sub-inTopic8"
+  val ResultsTopic8   = "pub-sub-outTopic8"
+
+  val singletonName9  = "pub-sub-master9"
+  val singletonRole9  = "pub-sub-back-end9"
+  val inTopic9        = "pub-sub-inTopic9"
+  val ResultsTopic9   = "pub-sub-outTopic9"
+
+  val singletonName10  = "pub-sub-master10"
+  val singletonRole10  = "pub-sub-back-end10"
+  val inTopic10        = "pub-sub-inTopic10"
+  val ResultsTopic10   = "pub-sub-outTopic10"
+
+  val singletonName11  = "pub-sub-master11"
+  val singletonRole11  = "pub-sub-back-end11"
+  val inTopic11        = "pub-sub-inTopic11"
+  val ResultsTopic11   = "pub-sub-outTopic11"
+
+  val singletonName12  = "pub-sub-master12"
+  val singletonRole12  = "pub-sub-back-end12"
+  val inTopic12        = "pub-sub-inTopic12"
+  val ResultsTopic12   = "pub-sub-outTopic12"
+
 }
--- src/main/scala/cluster/client/qing/AppConfig.scala
+++ src/main/scala/cluster/client/qing/AppConfig.scala
@@ -8,13 +8,64 @@ object AppConfig {
 
   val frontEndPortRange = 3000 to 3999
 
-  val singletonName1 = "master1"
-  val singletonRole1 = "back-end1"
-  val inTopic1 = "inTopic1"
-  val ResultsTopic1 = "outTopic1"
+  val singletonName1  = "queuing-master1"
+  val singletonRole1  = "queuing-back-end1"
+  val inTopic1        = "queuing-inTopic1"
+  val ResultsTopic1   = "queuing-outTopic1"
 
-  val singletonName2 = "master2"
-  val singletonRole2 = "back-end2"
-  val inTopic2 = "inTopic2"
-  val ResultsTopic2 = "outTopic2"
+  val singletonName2  = "queuing-master2"
+  val singletonRole2  = "queuing-back-end2"
+  val inTopic2        = "queuing-inTopic2"
+  val ResultsTopic2   = "queuing-outTopic2"
+
+  val singletonName3  = "queuing-master3"
+  val singletonRole3  = "queuing-back-end3"
+  val inTopic3        = "queuing-inTopic3"
+  val ResultsTopic3   = "queuing-outTopic3"
+
+  val singletonName4  = "queuing-master4"
+  val singletonRole4  = "queuing-back-end4"
+  val inTopic4        = "queuing-inTopic4"
+  val ResultsTopic4   = "queuing-outTopic4"
+
+  val singletonName5  = "queuing-master5"
+  val singletonRole5  = "queuing-back-end5"
+  val inTopic5        = "queuing-inTopic5"
+  val ResultsTopic5   = "queuing-outTopic5"
+
+  val singletonName6  = "queuing-master6"
+  val singletonRole6  = "queuing-back-end6"
+  val inTopic6        = "queuing-inTopic6"
+  val ResultsTopic6   = "queuing-outTopic6"
+
+  val singletonName7  = "queuing-master7"
+  val singletonRole7  = "queuing-back-end7"
+  val inTopic7        = "queuing-inTopic7"
+  val ResultsTopic7   = "queuing-outTopic7"
+
+  val singletonName8  = "queuing-master8"
+  val singletonRole8  = "queuing-back-end8"
+  val inTopic8        = "queuing-inTopic8"
+  val ResultsTopic8   = "queuing-outTopic8"
+
+  val singletonName9  = "queuing-master9"
+  val singletonRole9  = "queuing-back-end9"
+  val inTopic9        = "queuing-inTopic9"
+  val ResultsTopic9   = "queuing-outTopic9"
+
+  val singletonName10  = "queuing-master10"
+  val singletonRole10  = "queuing-back-end10"
+  val inTopic10        = "queuing-inTopic10"
+  val ResultsTopic10   = "queuing-outTopic10"
+
+  val singletonName11  = "queuing-master11"
+  val singletonRole11  = "queuing-back-end11"
+  val inTopic11        = "queuing-inTopic11"
+  val ResultsTopic11   = "queuing-outTopic11"
+
+  val singletonName12  = "queuing-master12"
+  val singletonRole12  = "queuing-back-end12"
+  val inTopic12        = "queuing-inTopic12"
+  val ResultsTopic12   = "queuing-outTopic12"
+
 }
